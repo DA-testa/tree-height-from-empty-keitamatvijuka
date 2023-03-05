eea96335c6f1db458fbfefcf99f7b5e6385b1913
keitamatvijuka <125823796+keitamatvijuka@users.noreply.github.com>
2023-03-05 23:25:18 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,29 +1,31 @@
 # python3
 #Keita Matvijuka 13. grupa Apl. nr. 221RDB506
 
+
 import sys
 import threading
 
 
 def compute_height(num_nodes, parent_indices):
-    nodes = [[] for _ in range(num_nodes)]
-    root = None
+    mezgls = [[] for _ in range(num_nodes)]
+    sakne = None
     for i, parent_index in enumerate(parent_indices):
         if parent_index == -1:
-            root = i
+            sakne = i
         else:
-            nodes[parent_index].append(i)
-    max_height = calculate_max_node_height(nodes, root)
+            mezgls[parent_index].append(i)
+    max_height = calculate_max_node_height(mezgls, sakne)
     return max_height
 
 
-def calculate_max_node_height(nodes, root):
-    stack = [(root, 1)]
+
+def calculate_max_node_height(mezgls, sakne):
+    stack = [(sakne, 1)]
     max_height = 1
     while stack:
-        node, height = stack.pop()
+        mezgls, height = stack.pop()
         max_height = max(max_height, height)
-        for child in nodes[node]:
+        for child in nodes[mezgls]:
             stack.append((child, height + 1))
     return max_height
 
@@ -36,7 +38,7 @@ def main():
             parent_indices = list(map(int, input().split()))
             max_height = compute_height(num_nodes, parent_indices)
             print(max_height)
-        if "F" in user_input:
+        elif "F" in user_input:
             file_path = "test/" + input()
             with open(file_path, 'r') as f:
                 num_nodes = int(f.readline().strip())
